f43cf0d3813fe972f82656bca5ff587cb2c480e9
ririndeismillia <ririndeismillia@gmail.com>
2026-03-02 20:57:12 +0700

    commit4

--- fungsi.cpp
+++ fungsi.cpp
@@ -3,3 +3,15 @@
 using namespace std;
 //deklarasi variabel
 int p, l;
+//deklarasi variabel 
+int p, l;
+//implementasi fungsi dan prosedur
+void input(){
+    cout << "Masukkan Panjang : ";
+    cin >> p;
+    cout << "Masukkan Lebar : ";
+    cin >> l;
+}
+int luasPersegi(){
+    return p * l;
+}
